b22a22d5db82dba4be5edb88f47a801e124fb09c
Hemavyas <hamualladi@gmail.com>
2024-09-04 18:04:37 +0530

    face_detection

--- facedetection.py
+++ facedetection.py
@@ -9,7 +9,7 @@ for image in all_images:
     faces=detect.detectMultiScale(gray_img,1.1,5)
     
     for(x,y,w,h) in faces:
-        final_img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        final_img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
     
     cv2.imshow("group photo",final_img)
     cv2.waitKey(10000)
